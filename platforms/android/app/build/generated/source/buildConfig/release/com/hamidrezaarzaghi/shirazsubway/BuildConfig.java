04ce162c5b7011548096d42aa541bbb8cf5239fd
HamidRezaArzaghi <Arzaghi@outlook.com>
2018-07-31 23:01:37 +0430

    Version 1.0 Released

---
+++ platforms/android/app/build/generated/source/buildConfig/release/com/hamidrezaarzaghi/shirazsubway/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hamidrezaarzaghi.shirazsubway;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.hamidrezaarzaghi.shirazsubway";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1000;
+  public static final String VERSION_NAME = "1.0";
+}
