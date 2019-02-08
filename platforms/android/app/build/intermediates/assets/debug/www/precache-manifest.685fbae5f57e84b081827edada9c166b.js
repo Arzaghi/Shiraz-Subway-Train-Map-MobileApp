268bec4c3a5050167b53441c2e520a09e110c475
Hamid Reza Arzaghi <arzaghi@outlook.com>
2019-02-08 14:09:44 +0330

    Version 1.2

---
+++ platforms/android/app/build/intermediates/assets/debug/www/precache-manifest.685fbae5f57e84b081827edada9c166b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66b5fea81937a8d35b6c",
+    "url": "/static/js/main.66b5fea8.chunk.js"
+  },
+  {
+    "revision": "01b1c8c6716d4daaecf8",
+    "url": "/static/js/1.01b1c8c6.chunk.js"
+  },
+  {
+    "revision": "01b1c8c6716d4daaecf8",
+    "url": "/static/css/1.680c1ef2.chunk.css"
+  },
+  {
+    "revision": "22ee8a74ae316cc6737527324f9e241c",
+    "url": "/index.html"
+  }
+];
---
+++ www/precache-manifest.379ca7d850d22f8cbe8144738503f989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "152df63021e624a2658b",
+    "url": "/static/js/main.152df630.chunk.js"
+  },
+  {
+    "revision": "01b1c8c6716d4daaecf8",
+    "url": "/static/js/1.01b1c8c6.chunk.js"
+  },
+  {
+    "revision": "01b1c8c6716d4daaecf8",
+    "url": "/static/css/1.680c1ef2.chunk.css"
+  },
+  {
+    "revision": "3ebe8e44a0fe99ec793e8a8e4ad10f9f",
+    "url": "/index.html"
+  }
+];
